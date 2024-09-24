0cb551f70b28d31b336886bea3320ee0f0ee12be
solomon Abate <solomonabate18@gmail.com>
2024-09-24 09:39:56 +0300

    Update data.txt

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import json
 def text_to_list():
     array = []
     while True:
@@ -9,3 +10,24 @@ def text_to_list():
         except:
             break
     print(array)
+
+
+def is_valid_json_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            parsed_json = json.load(file)  
+    except ValueError as e:
+        return False
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+        return False
+    return parsed_json
+
+
+file_path = 'docs/data.txt'
+parsed_json = is_valid_json_file(file_path)
+if is_valid_json_file(file_path):
+    print("The file contains valid JSON.")
+    print(parsed_json)
+else:
+    print("The file does not contain valid JSON.")
